a5d454b368048c789c8c8d7f33e83643caa5998d
manuj <manujdixit04@gmail.com>
2024-04-03 11:37:11 +0530

    balance

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,39 @@
+import { useEffect, useState } from "react";
 import { Appbar } from "../components/Appbar";
 import { Balance } from "../components/Balance";
 import { Users } from "../components/Users";
+import axios from "axios";
 
 const Dashboard = () => {
+  const [balance, setbalance] = useState(0);
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4001/api/v1/account/balance",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.token}`,
+            },
+          }
+        );
+        // balance = response.balance;
+        console.log("balance", response);
+        setbalance(response.data.balance);
+      } catch (error) {
+        console.log("error fetching balance", error);
+      }
+    };
+
+    fetchBalance();
+  });
+
   return (
     <div>
       <Appbar />
       <div className="m-8">
-        <Balance value={"10,000"} />
+        <Balance value={balance} />
         <Users />
       </div>
     </div>
